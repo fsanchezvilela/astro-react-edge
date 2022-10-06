200339cc6e25df2218b2380fd4efa0ed94af76bb
Fernando Sánchez <f.sanchezvilela@gmail.com>
2022-10-06 10:03:45 +0200

    feat: add react partial hydratation and use edge functions

---
+++ src/components/react/Button.tsx
@@ -0,0 +1,20 @@
+import type { ReactNode } from 'react';
+
+export interface ButtonProps {
+  children: ReactNode;
+}
+
+const Button = ({ children }: ButtonProps): ReactNode => {
+  const handleOnClick = () => alert('you click a SSR React component');
+  return (
+    <button
+      className="mt-4 text-4xl font-extrabold tracking-tight text-slate-900  rounded-lg border-2 border-solid border-slate-300 p-2 m-2"
+      onClick={handleOnClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+Button.displayName = 'Button';
+export default Button;
